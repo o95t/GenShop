144e69c00de93c9c9059f2d956c604286ff5a65e
Mahmoud Elgendy <mahmoudelgendy1@gmail.com>
2024-03-09 21:05:36 -0600

    create dto

---
+++ sources/GenShop/API/Dtos/EmployeeDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Dtos
+{
+    public class EmployeeDto
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public String FirstName { get; set; }
+        [MaxLength(30)]
+        public String? MiddleName { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public String LastName { get; set; }
+        [Required]
+        [Range(0, 1000000)]
+        public Decimal Salary { get; set; }
+        public int? ManagerId { get; set; }
+    }
+}
