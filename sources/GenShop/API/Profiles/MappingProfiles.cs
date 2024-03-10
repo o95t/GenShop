aabcd9d9d21926204644ad3e9b12f952f240aae1
Mahmoud Elgendy <mahmoudelgendy1@gmail.com>
2024-03-09 21:17:20 -0600

    automapper

--- sources/GenShop/API/Profiles/MappingProfiles.cs
+++ sources/GenShop/API/Profiles/MappingProfiles.cs
@@ -1,8 +1,15 @@
-﻿using AutoMapper;
+﻿using API.Dtos;
+using AutoMapper;
+using Core.Entities;
 
 namespace API.Profiles
 {
     public class MappingProfiles : Profile
     {
+        public MappingProfiles()
+        {
+            CreateMap<Employee, EmployeeDto>();
+            CreateMap<EmployeeDto, Employee>();
+        }
     }
 }
