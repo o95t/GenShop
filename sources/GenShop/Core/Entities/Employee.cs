846a65f9105188579f66cfca127f02d1a768828b
Mahmoud Elgendy <mahmoudelgendy1@gmail.com>
2024-03-09 20:11:46 -0600

    Create db context

---
+++ sources/GenShop/Core/Entities/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public String FirstName { get; set; }
+        public String? MiddleName { get; set; }
+        public String LastName { get; set; }
+        public Decimal Salary { get; set; }
+        public int? ManagerId { get; set; }
+        public Employee? Manager { get; set; }
+    }
+}
---
+++ sources/GenShop/Infrastructure/Data/EmployeeContext.cs
@@ -0,0 +1,37 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+        {
+
+        }
+        public DbSet<EmployeeContext> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Employee>()
+                .Property(p => p.FirstName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            modelBuilder.Entity<Employee>()
+                .Property(p => p.MiddleName)
+                .HasMaxLength(50);
+
+            modelBuilder.Entity<Employee>()
+                .Property(p => p.LastName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            modelBuilder.Entity<Employee>()
+                .Property(p => p.Salary)
+                .HasPrecision(9, 2)
+                .HasAnnotation("MaxLength", 1000000);
+        }
+    }
+}
