a0c35d91b579d196616006543e11cfa9468ed379
Mahmoud Elgendy <mahmoudelgendy1@gmail.com>
2024-03-09 20:15:48 -0600

    Create connection string and register sqlserver

--- sources/GenShop/API/Program.cs
+++ sources/GenShop/API/Program.cs
@@ -1,9 +1,14 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<EmployeeContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
